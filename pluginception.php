c6346f2651e6178ad3456b3def1dacf242bd3e6f
Otto42 <Otto42@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-12-17 14:44:06 +0000

    add copyright comment
    git-svn-id: https://plugins.svn.wordpress.org/pluginception/trunk@823976 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- pluginception.php
+++ pluginception.php
@@ -9,6 +9,23 @@ Author URI: http://ottopress.com
 Text Domain: pluginception
 License: GPLv2 only
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+    Copyright 2011-2013  Samuel Wood  (email : otto@ottodestruct.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License version 2,
+    as published by the Free Software Foundation.
+
+    You may NOT assume that you can use any other version of the GPL.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    The license for this software can likely be found here:
+    http://www.gnu.org/licenses/gpl-2.0.html
+
 */
 
 // Load the textdomain
